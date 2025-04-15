3dd4657260b98dab823f84611f7aa8ef200a5739
Paul Baumgärtner <paulbaumi07@gmail.com>
2025-04-15 18:58:25 +0200

    Initial Commit

---
+++ DialogueResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace DealOverviewMod
+{
+    public class DialogueResponse
+    {
+        public string Id;
+
+        public string Text;
+
+        public Action Action;
+
+        public DialogueResponse[] NextResponses;
+
+        public bool DisableDefaultBehavior;
+
+        public DialogueResponse(string id, string text, Action action, bool disableDefaultBehavior = true, DialogueResponse[] next = null)
+        {
+            Id = id;
+            Text = text;
+            Action = action;
+            DisableDefaultBehavior = disableDefaultBehavior;
+            NextResponses = next;
+        }
+    }
+}
